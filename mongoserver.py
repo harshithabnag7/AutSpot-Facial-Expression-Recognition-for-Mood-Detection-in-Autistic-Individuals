0d1bcd190b768ba523c9c80c6d44cbaeb8ada5b2
Harshitha B Nagaraj <72066690+harshithabnag7@users.noreply.github.com>
2023-02-27 21:15:20 -0500

    Update api keys

--- mongoserver.py
+++ mongoserver.py
@@ -4,7 +4,7 @@ from flask import Flask,redirect
 from flask import render_template
 app = Flask(__name__)
 from pymongo import MongoClient
-client = MongoClient('mongodb+srv://rahulgowda4785:3t0KDthxRyiDrg85@autspot.u42dncc.mongodb.net/?retryWrites=true&w=majority')
+client = MongoClient('mongodb+srv://<USERNAME>:<PASSWORD>autspot.u42dncc.mongodb.net/?retryWrites=true&w=majority')
 dbname = client['Autism']
 collection_name = dbname["Userdata"]
 
