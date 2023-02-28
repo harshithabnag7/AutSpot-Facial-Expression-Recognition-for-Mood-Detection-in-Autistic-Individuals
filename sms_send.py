1b746657c344013bf1d58afff485d3b225f7ac95
Harshitha B Nagaraj <72066690+harshithabnag7@users.noreply.github.com>
2023-02-27 21:11:33 -0500

    Update sms_send.py

--- sms_send.py
+++ sms_send.py
@@ -1,6 +1,6 @@
 from twilio.rest import Client
-account_sid = "AC67c7e90f107213ca1855d2fd2d1c9e4d"
-auth_token = "1ad4093c2c16fe49c02ea8567c9a8484"
+account_sid = "ACCOUNT-SID"
+auth_token = "AUTH_TOKEN"
 client = Client(account_sid, auth_token)
 def send_alert(emg_contact):
     message = client.messages.create(body = "Your contact is having an autism  episode", from_ = '+18559254969', to = emg_contact)
